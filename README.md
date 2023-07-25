a369df9da13837496658e334b887270f882e046c
faizallramadhan <faizal.ramadhan007@gmail.com>
2023-07-25 14:18:28 +0700

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # evermos-test
+
+Requirements: K6, K6 Reporter<br>
+To run the tests you need to have installed k6 and its reporter.<br><br>
+
+Step to run:<br>
+1. clone this repo<br>
+2. go to the evermos-test folder<br>
+3. open folder in vscode<br>
+4. for task no. 1 kindly open integration.js and run it with "k6 run integration.js -o json=integration.json" command, it will automatically run and generate the integration.json and html report, or you can also run only with "k6 run integration.js" command<br>
+5. for task no. 2 and 3 kindly open performance.js and run it with "k6 run performance.js -o json=performance.json" it will automatically run and generate the integration.json and html report.
+
+
+Credit: Faizal Ramadhan Suyitno Putra
---
+++ performance.js
@@ -0,0 +1,46 @@
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
+const api1BaseUrl = 'https://reqres.in';
+const api1endpoints = '/api/users';
+const api2endpoints = '/api/users/2';
+
+export let options = {
+  vus: 1000,
+  iterations: 3500,
+  thresholds: {
+    http_req_duration: ['p(95)<2000'],
+  },
+};
+
+export default function () {
+  group('Performance Test', function () {
+    let payload = JSON.stringify({ "name": "morpheus", "job": "leader" });
+    let headers = { "Content-Type": "application/json" };
+    let postResponse = http.post(`${api1BaseUrl}${api1endpoints}`, payload, { headers: headers });
+    check(postResponse, {
+      'POST is successful': (res) => res.status === 201,
+      'POST response contains ID': (res) => res.json().hasOwnProperty('id'),
+    });
+
+    let userId = postResponse.json().id;
+    payload = JSON.stringify({ "name": "morpheus", "job": "zion resident" });
+    let putResponse = http.put(`${api1BaseUrl}${api2endpoints}`, payload, { headers: headers });
+    check(putResponse, {
+      'PUT is successful': (res) => res.status === 200,
+      'PUT response contains updated job': (res) => res.json().job === 'zion resident',
+    });
+  });
+}
+
+  sleep(0.1);
+  
+export function handleSummary(data) {
+  return {
+    "performance-testResult.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
+}
+
